8eed5aafceb1631d8e21f48fbe877fced86dd165
Shankar Govindarajan <shgovind@redhat.com>
2018-12-09 22:38:35 -0600

    This is not working

--- service-frontend/src/main/java/com/sample/frontend/FrontEndApplication.java
+++ service-frontend/src/main/java/com/sample/frontend/FrontEndApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
-@RibbonClient(name = "service-backend", configuration = RibbonConfiguration.class)
 public class FrontEndApplication {
 
     @Bean
